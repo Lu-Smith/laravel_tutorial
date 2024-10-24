848fa9f9bf57d8a1a338c5f0361eef6a7ade9492
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-24 10:18:45 +0100

    Changed route::post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +11,12 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::post('/contact', function () {
+Route::post('/contact', function (Request $request) {
+    $validated = $request->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'message' => 'required|min:8|max:1000',
+    ]);
+
     return redirect('/');
 });
